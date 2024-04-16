72e51b5167fafb011c80864c9c0c15fc08712a44
victo <victo@DESKTOP-VDD4G5Q>
2024-04-16 09:46:00 +0200

    Solución ejercicio 1

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -1,24 +1,24 @@
 package ejercicio1;
 
 public class Calculadora {
-	public int sumar(int a, int b) {
-		int resultado = a + b;
+	public int sumar(int sumando1, int sumando2) {
+		int resultado = sumando1 + sumando2;
 		return resultado;
 	}
 
-	public int restar(int a, int b) {
-		int resultado = a - b;
+	public int restar(int minuendo, int sustraendo) {
+		int resultado = minuendo - sustraendo;
 		return resultado;
 	}
 
-	public int multiplicar(int primerFactor, int segundoFactor) {
-		int producto = primerFactor * segundoFactor;
+	public int multiplicar(int factor1, int factor2) {
+		int producto = factor1 * factor2;
 		return producto;
 	}
 
 	public int dividir(int dividendo, int divisor) {
 		if (divisor == 0) {
-			System.out.println("Error: No se puede dividir por cero.");
+			// TODO DIVISION POR CERO; ¿LANZAR EXCEPCION?
 			return 0;
 		} else {
 			int cociente = dividendo / divisor;
@@ -26,14 +26,4 @@ public class Calculadora {
 		}
 	}
 
-
-
-	public static void main(String[] args) {
-		Calculadora calc = new Calculadora();
-
-		System.out.println("Suma: " + calc.sumar(10, 5));
-		System.out.println("Resta: " + calc.restar(10, 5));
-		System.out.println("Multiplicación: " + calc.multiplicar(10, 5));
-		System.out.println("División: " + calc.dividir(10, 5));
-	}
 }
